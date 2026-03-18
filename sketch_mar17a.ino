1c42f875250e4770706dce92021653e0ddb378c7
Daniel Teixeira <daniel.teixeira.corporativo@gmail.com>
2026-03-18 13:49:31 -0300

    index

--- sketch_mar17a.ino
+++ sketch_mar17a.ino
@@ -1,16 +1,20 @@
 #include "ibus.h"
 
-// Seus pinos (mantidos)
+// Seus pinos analógicos (mantidos)
 #define RIGHT_VRX A0
 #define RIGHT_VRY A1
 #define LEFT_VRX A3
 #define LEFT_VRY A4
 
-// Define os analógicos usados (IMPORTANTE)
+// Pinos digitais para teste
+#define PIN5 5
+#define PIN6 6
+
+// Define os analógicos usados
 byte analogPins[] = {RIGHT_VRX, RIGHT_VRY, LEFT_VRX, LEFT_VRY};
 
-// Sem botões
-byte digitalPins[] = {};
+// Agora com digitais
+byte digitalPins[] = {PIN5, PIN6};
 byte digitalBitmappedPins[] = {};
 
 // Configuração
@@ -29,6 +33,10 @@ IBus ibus(NUM_CHANNELS);
 void setup() {
   analogReference(ANALOG_REFERENCE);
   Serial.begin(BAUD_RATE);
+
+  // Configura os pinos digitais
+  pinMode(PIN5, INPUT_PULLUP);
+  pinMode(PIN6, INPUT_PULLUP);
 }
 
 void loop() {
@@ -36,10 +44,21 @@ void loop() {
 
   ibus.begin();
 
-  // Envia os 4 eixos
+  // Envia os analógicos
   for(int i = 0; i < ANALOG_INPUTS_COUNT; i++) {
     int val = analogRead(analogPins[i]);
     ibus.write(1000 + (uint32_t)val * 1000 / 1023);
+  }
+
+  // Envia os digitais (0 ou 1)
+  for(int i = 0; i < DIGITAL_INPUTS_COUNT; i++) {
+    int val = digitalRead(digitalPins[i]);
+
+    // Converte para padrão iBus (1000 ou 2000)
+    if(val == HIGH)
+      ibus.write(2000);
+    else
+      ibus.write(1000);
   }
 
   ibus.end();
