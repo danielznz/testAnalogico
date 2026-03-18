f62485e74ad4fe28960ebb6bec94e8b4634beaab
Daniel Teixeira <daniel.teixeira.corporativo@gmail.com>
2026-03-18 16:13:26 -0300

    pin-digital

--- sketch_mar17a.ino
+++ sketch_mar17a.ino
@@ -1,18 +1,37 @@
 #include "ibus.h"
 
-// Analógicos
+// Seus pinos (mantidos)
 #define RIGHT_VRX A0
 #define RIGHT_VRY A1
 #define LEFT_VRX A3
 #define LEFT_VRY A4
 
+// --- DECLARAÇÃO DOS PINOS DIGITAIS ---
+#define D2_PIN  2
+#define D3_PIN  3
+#define D4_PIN  4
+#define D5_PIN  5
+#define D6_PIN  6
+#define D7_PIN  7
+#define D8_PIN  8
+#define D9_PIN  9
+#define D10_PIN 10
+#define D11_PIN 11
+#define D12_PIN 12
+
+// Analógicos
 byte analogPins[] = {RIGHT_VRX, RIGHT_VRY, LEFT_VRX, LEFT_VRY};
 
-// Digitais (D2 até D10)
-byte digitalPins[] = {2,3,4,5,6,7,8,9,10};
+// Digitais (agora com todos)
+byte digitalPins[] = {
+  D2_PIN, D3_PIN, D4_PIN, D5_PIN, D6_PIN,
+  D7_PIN, D8_PIN, D9_PIN, D10_PIN,
+  D11_PIN, D12_PIN
+};
+
 byte digitalBitmappedPins[] = {};
 
-// Config
+// Configuração
 #define ANALOG_REFERENCE DEFAULT
 #define BAUD_RATE 115200
 #define UPDATE_INTERVAL 10
@@ -29,7 +48,7 @@ void setup() {
   analogReference(ANALOG_REFERENCE);
   Serial.begin(BAUD_RATE);
 
-  // Configura todos os digitais
+  // Configura todos os pinos como INPUT_PULLUP
   for(int i = 0; i < DIGITAL_INPUTS_COUNT; i++) {
     pinMode(digitalPins[i], INPUT_PULLUP);
   }
@@ -40,21 +59,21 @@ void loop() {
 
   ibus.begin();
 
-  // Analógicos
+  // Envia os 4 eixos
   for(int i = 0; i < ANALOG_INPUTS_COUNT; i++) {
     int val = analogRead(analogPins[i]);
     ibus.write(1000 + (uint32_t)val * 1000 / 1023);
   }
 
-  // Digitais como botões
+  // --- BOTÕES (D2 até D12) ---
   for(int i = 0; i < DIGITAL_INPUTS_COUNT; i++) {
     int val = digitalRead(digitalPins[i]);
 
     // LOW = pressionado
     if(val == LOW)
-      ibus.write(2000);
+      ibus.write(2000); // botão ON
     else
-      ibus.write(1000);
+      ibus.write(1000); // botão OFF
   }
 
   ibus.end();
@@ -63,20 +82,3 @@ void loop() {
   if(time < UPDATE_INTERVAL)
     delay(UPDATE_INTERVAL - time);
 }
-
-for(int i = 0; i < DIGITAL_INPUTS_COUNT; i++) {
-  int val = digitalRead(digitalPins[i]);
-
-  Serial.print("Pino D");
-  Serial.print(digitalPins[i]);
-  Serial.print(": ");
-
-  if(val == LOW)
-    Serial.print("PRESSIONADO");
-  else
-    Serial.print("SOLTO");
-
-  Serial.print(" | ");
-}
-
-Serial.println();
